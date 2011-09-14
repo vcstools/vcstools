871dfdce922abf7dc65496337382515f28b5210b
Ken Conley <kwc@willowgarage.com>
2011-09-14 16:48:37 -0700

    prepping 0.1.0 push

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='vcstools',
-      version= '0.0.3',
+      version= '0.1.0',
       packages=['vcstools'],
       package_dir = {'':'src'},
       scripts = [],
