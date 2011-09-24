fca1ba556f143396466d6208d9a58eb37f5286d1
Thibault Kruse <kruset@in.tum.de>
2011-09-24 21:41:39 +0200

    test case using different branch than master

--- test/test_git.py
+++ test/test_git.py
@@ -58,6 +58,7 @@ class GitClientTestSetups(unittest.TestCase):
         subprocess.check_call(["git", "add", "*"], cwd=self.remote_path)
         subprocess.check_call(["git", "commit", "-m", "initial"], cwd=self.remote_path)
         subprocess.check_call(["git", "tag", "test_tag"], cwd=self.remote_path)
+        subprocess.check_call(["git", "branch", "test_branch"], cwd=self.remote_path)
         
         po = subprocess.Popen(["git", "log", "-n", "1", "--pretty=format:\"%H\""], cwd=self.remote_path, stdout=subprocess.PIPE)
         self.readonly_version_init = po.stdout.read().rstrip('"').lstrip('"')
@@ -152,7 +153,7 @@ class GitClientTest(GitClientTestSetups):
         self.assertEqual(client.get_version(), new_version)
 
 
-    def test_checkout_specific_branch_and_update(self):
+    def test_checkout_master_branch_and_update(self):
         from vcstools.git import GitClient
         subdir = "checkout_specific_version_test"
         local_path = os.path.join(self.root_directory, "ros")
@@ -168,9 +169,29 @@ class GitClientTest(GitClientTestSetups):
         self.assertEqual(client.get_url(), url)
         self.assertEqual(client.get_branch_parent(), branch)
         
+        self.assertTrue(client.update(branch))
+        self.assertEqual(client.get_branch_parent(), branch)
+
+
+    def test_checkout_specific_branch_and_update(self):
+        from vcstools.git import GitClient
+        subdir = "checkout_specific_version_test"
+        local_path = os.path.join(self.root_directory, "ros")
+        url = self.remote_path
+        branch = "test_branch"
+        client = GitClient(local_path)
+        self.assertFalse(client.path_exists())
+        self.assertFalse(client.detect_presence())
+        self.assertTrue(client.checkout(url, branch))
+        self.assertTrue(client.path_exists())
+        self.assertTrue(client.detect_presence())
+        self.assertEqual(client.get_path(), local_path)
+        self.assertEqual(client.get_url(), url)
+        self.assertEqual(client.get_branch_parent(), branch)
+        
         new_branch = 'master'
-        self.assertTrue(client.update(new_branch))
-        self.assertEqual(client.get_branch_parent(), new_branch)
+        self.assertFalse(client.update(new_branch))
+        self.assertEqual(client.get_branch_parent(), branch)
 
 
     def test_checkout_specific_tag_and_update(self):
