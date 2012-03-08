295d842057df1a1d99152f5afb4a5fc53ac4e70b
Tully Foote <tfoote@willowgarage.com>
2012-03-07 16:06:03 -0800

    vcstools 0.1.8

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='vcstools',
-      version= '0.1.7',
+      version= '0.1.8',
       packages=['vcstools'],
       package_dir = {'':'src'},
       scripts = [],
