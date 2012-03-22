9952c75416f3b3ac9dd5df7c7810bd5858c48087
Tully Foote <tfoote@willowgarage.com>
2012-03-21 19:47:45 -0700

    0.1.12

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='vcstools',
-      version= '0.1.11',
+      version= '0.1.12',
       packages=['vcstools'],
       package_dir = {'':'src'},
       scripts = [],
