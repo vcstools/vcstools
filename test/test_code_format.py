1ef0a2d2d0180c869148ea63062ae46e29dd4315
Tully Foote <tfoote@willowgarage.com>
2013-06-14 16:58:12 -0700

    fixing python3 compatability

--- test/test_code_format.py
+++ test/test_code_format.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import os
 from pkg_resources import parse_version, get_distribution
 
@@ -10,14 +11,14 @@ def test_pep8_conformance():
     try:
         import pep8
     except:
-        print "Skipping pep8 Tests because pep8.py not installed."
+        print("Skipping pep8 Tests because pep8.py not installed.")
         return
 
     # Skip test if pep8 is not new enough
     pep8_version = parse_version(get_distribution('pep8').version)
     needed_version = parse_version('1.0')
     if pep8_version < needed_version:
-        print "Skipping pep8 Tests because pep8.py is too old"
+        print("Skipping pep8 Tests because pep8.py is too old")
         return
 
     pep8style = pep8.StyleGuide(max_line_length=120)
